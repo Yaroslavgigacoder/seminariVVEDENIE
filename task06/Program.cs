6a956e475574edc3cc3199699d077af67f453782
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-10-15 21:30:02 +0300

    Решил задачу 06

---
+++ task06/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите 1 число:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b;
+if (a % 2 == 0)
+{ 
+    Console.Write("Число четное");
+}
+else
+{
+    Console.Write("Число нечетное");
+}
