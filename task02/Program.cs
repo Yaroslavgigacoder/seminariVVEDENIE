5f5a4c277a948260a81b877676f0d4a7993566a4
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-10-15 21:50:40 +0300

    все дз решены

---
+++ task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите 2 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a<b)
+{
+    Console.Write($"Большее число {b} ");
+    Console.Write($"Меньшее число {a} ");
+}
+else
+{
+Console.Write($"Большее число {a} ");
+Console.Write($"Меньшее число {b} ");
+}
