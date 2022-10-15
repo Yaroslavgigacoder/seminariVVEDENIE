74d2638ecedea78cd18ada89c7e401e8f8879c45
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-10-15 20:10:38 +0300

    решил дз 2 4

---
+++ task04/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите 3 числа:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b && a>c)
+{
+    Console.WriteLine($"Максимальное число {a} ");
+}
+if (b>a && b>c)
+{
+    Console.WriteLine($"Максимальное число {b} ");
+}
+if (c>b && c>a)
+{
+    Console.WriteLine($"Максимальное число {c} ");
+}
