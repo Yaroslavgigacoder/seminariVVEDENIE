58aed0a062f41ac276aa2aeee91c8f5be80e7ccc
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-10-23 21:31:28 +0300

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Нет");
+        break;
+    case 6:
+    case 7:
+        Console.WriteLine("Да");
+        break;
+    default:
+        Console.WriteLine("Такого дня недели нет");
+        break;
+}
