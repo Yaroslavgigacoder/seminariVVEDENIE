985bfd2200c64b0257395a12784034b7ca18b6fe
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-10-23 20:49:27 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,7 @@
+﻿int SecondNumber(int number)
+{
+    int firstNumber = number /100;
+    return (number - (firstNumber*100)) / 10; 
+}
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SecondNumber(num));
