35a1a92c63aa96d6823921df06386150c7910e61
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-10-23 21:06:07 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,10 @@
+﻿int Number(int number)
+{
+    while(number>1000)
+    {
+        number = number / 10;
+    }   
+    return number % 10;
+}
+int  num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Number(num));
