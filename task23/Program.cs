5d2f3a2f998d1e843f4f3ee9f3de73535e28dbe5
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-10-27 19:15:01 +0300

    task 23

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+void table(int n)
+{
+    if( n > 0 )
+    {
+        int current = 1;
+        while (current <= n)
+        {
+            Console.WriteLine($"{current,5}|{current * current * current,5}");
+            current++;
+        }
+        
+
+    }
+    else Console.WriteLine("Некоррекное чилсо");
+}
+table(n);
