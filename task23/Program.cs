f0b598b1fdb39cd3901f97603896206e3ec64580
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-05 02:25:34 +0300

    task23

--- task23/Program.cs
+++ task23/Program.cs
@@ -2,17 +2,16 @@
 int n = Convert.ToInt32(Console.ReadLine());
 void table(int n)
 {
-    if( n > 0 )
+    if (n > 0)
     {
         int current = 1;
         while (current <= n)
         {
             Console.WriteLine($"{current,5}|{current * current * current,5}");
             current++;
-        }
         
-
+        }
     }
-    else Console.WriteLine("Некоррекное чилсо");
+    else Console.WriteLine("Некорректное число");
 }
 table(n);
