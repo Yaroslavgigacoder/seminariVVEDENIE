d7fc49563d29a9afe117804bfa9b7d39086ba244
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-05 01:49:17 +0300

    task 19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int y = n;
+int revers = 0;
+if (n>9999 && n < 100000)Console.Write(Polindrom(n) == n ? "Да" : "Нет");
+else Console.WriteLine("Число не пятизначное");
+int Polindrom(int newnum)
+{
+    while (y > 0)
+    {
+        newnum = y % 10;
+        revers = (revers * 10) + newnum;
+        y = y / 10;
+    }
+    return revers;
+}
