942ecfd8f2588dca31b6b477f6b7ba52b9ddc6d5
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-13 20:36:13 +0300

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumNum(int num)
+{
+    int sum=0;
+    while (num > 0)
+    {
+        int newnum = num % 10;
+        sum +=newnum;
+        num = num / 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр заданного числа= {SumNum(number)}");
