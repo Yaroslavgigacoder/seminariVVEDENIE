18fa560f7ef05b56c3fa59ae6593211653995232
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-13 20:35:32 +0300

    task 25

---
+++ task25/Program.cs
@@ -0,0 +1,15 @@
+﻿int Prod(int A, int B)
+{
+    int voz = 1;
+    for (int i = 0; i < B; i++)
+    {
+        voz *= A;
+    }
+    return voz;
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int deg = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{Prod(num, deg)}");
