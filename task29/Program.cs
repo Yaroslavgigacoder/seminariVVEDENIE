2fde9f5f3042f0bb6365fc4d2afffb6c8ac56572
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-13 21:01:43 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+int[] arr = CreateArrayRndInt(8, -100, 100);
+PrintArray(arr);
