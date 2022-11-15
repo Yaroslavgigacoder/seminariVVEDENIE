62a736531f0dc4bdc205b5a204b948ab0375d609
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-15 21:53:29 +0300

    task34

---
+++ task34/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(99, 1000);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+int Even(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+
+}
+Console.Write("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(length);
+PrintArray(arr);
+int even = Even(arr);
+Console.WriteLine(even > 0 ? $"Колличество четных чисел в массиве = {even} " : "Четных чисел в массиве нет");
