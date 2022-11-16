d74f166a6b0684e27d413c7ffc7e6d7958072af7
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-17 01:00:10 +0300

    task36

---
+++ task36/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+int SummaUneven(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+
+}
+Console.Write("Введите количество элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(length);
+PrintArray(arr);
+int summa = SummaUneven(arr);
+Console.WriteLine($"Сумма элементов массива, стоящих на нечетных позициях = {summa}");
