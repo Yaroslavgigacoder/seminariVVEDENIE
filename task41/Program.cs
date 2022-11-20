b51c3e0ae1687b99d99d5d64eb369d5eba1e62e2
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-21 02:15:50 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] CreateArray(int len)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"Введите {i} элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+int Positiv(int[] array)
+{   
+    int count=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) count++;
+    }
+    return count;
+}
+Console.Write("Введите количество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(M);
+PrintArray(arr);
+Console.Write($"Колличество положительных чисел в массиве = {Positiv(arr)} ");
