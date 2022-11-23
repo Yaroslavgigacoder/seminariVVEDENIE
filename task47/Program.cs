fa92224425d19b29f7c05c2d8f2e332536f9164a
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-23 22:26:50 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,30 @@
+﻿double[,] CreateMatrixRndInt(int rows, int columns, int min,int max)
+{
+    double[,] matrix = new double[rows,columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rnd.NextDouble() * (max - min) + min;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{Math.Round(matrix[i,j],2), 5} |");
+            else Console.Write($"{Math.Round(matrix[i,j],2), 5}");
+        }       
+        Console.WriteLine("|");   
+    }
+}    
+double[,] array2D=CreateMatrixRndInt(3,4,-9,9);
+PrintMatrix(array2D);
