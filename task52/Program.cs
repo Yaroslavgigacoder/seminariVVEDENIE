b6c14163ca79f13574f9ce93887c9e54e1111349
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-24 02:13:14 +0300

    task52

---
+++ task52/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5} |");
+        }
+        Console.WriteLine("");
+    }
+}
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{Math.Round(array[i], 2),7} ");
+        if (i < array.Length - 1) Console.Write(",");
+    }
+    Console.WriteLine("]");
+}
+double[] SumElements(int[,] matrix)
+{
+    double[] mean = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        mean[j] = sum / matrix.GetLength(0);
+    }
+    return mean;
+}
+
+int[,] array2D = CreateMatrixRndInt(5, 2, 2, 8);
+PrintMatrix(array2D);
+double[] meanelements = SumElements(array2D);
+PrintArray(meanelements);
