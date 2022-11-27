541ad21ef345157c19d1cc7cc4a0b5ec1d992c27
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-11-28 00:12:28 +0300

    task54

---
+++ task54/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine("|");
+    }
+
+}
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1)-1; k++)
+            {
+                if(matrix[i,k]< matrix[i,k+1]) 
+                {
+                    int temp=0;
+                    temp=matrix[i,k];
+                    matrix[i,k]=matrix[i,k+1];
+                    matrix[i,k+1]=temp;
+                } 
+            }
+         
+        }
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -9, 9);
+PrintMatrix(array2D);
+SortMatrix(array2D);
+Console.WriteLine("");
+PrintMatrix(array2D);
