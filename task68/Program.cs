be638319b8db052c4742c02ee6d2640502f436f6
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-12-01 18:36:36 +0300

    task68

---
+++ task68/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите натуральное число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(Akkerman(m, n));
+
+int Akkerman(int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+    if (numM > 0 && numN == 0) return Akkerman(numM - 1, 1);
+    if (numM > 0 && numN > 0) return (Akkerman(numM - 1, Akkerman(numM, numN - 1)));
+    return Akkerman(numM,numN);
+}
