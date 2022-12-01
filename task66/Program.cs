f6fa4aaebd6a2f7797c8d4c53e32e4af86b16453
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-12-01 17:07:58 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите натуральное число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SummaNumbers(int numM,int numN,int sum)
+{   
+    if (numM>numN) 
+    {
+        Console.WriteLine($"Сумма равна {sum}");
+        return;
+    }
+    sum=sum+numM++;
+    SummaNumbers (numM,numN,sum);  
+}
+if(m<n) SummaNumbers(m,n,0);
+else SummaNumbers(n,m,0);
