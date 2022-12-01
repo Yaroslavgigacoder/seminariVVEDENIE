5a3761e4befa052d1f278218bbef0622334f21c0
Yaroslav.Gladkov <gladkov2000@inbox.ru>
2022-12-01 14:44:54 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+NaturalNumber(num);
+
+void NaturalNumber(int number)
+{
+    if (number==0) return;
+    Console.Write($"{number} ");
+    NaturalNumber(number-1);
+}
